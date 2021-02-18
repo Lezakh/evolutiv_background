134d899e8fbc79bb08edfd0b5459e677ea14ed3c
Ophélie De Sousa Oliveira <47571830+Lezakh@users.noreply.github.com>
2021-02-18 18:24:24 +0100

    Create evolutiv_background.js

---
+++ evolutiv_background.js
@@ -0,0 +1,94 @@
+jQuery(document).ready(function(){var date = new Date();
+                var heure = date.getHours();
+                if(location.pathname=="/f32-refectoire"){
+                  $("body").css("background-image", "url('http://i39.servimg.com/u/f39/11/74/65/03/refect10.jpg')");
+
+                }else if (location.pathname=="/f10-imperium"){
+                  $("body").css("background-image", "url('http://i39.servimg.com/u/f39/14/70/77/37/entrye11.jpg')");
+
+                }else if (location.pathname=="/f11-batiment-a"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/262317BatimentA.jpg')");
+
+                }else if (location.pathname=="/f37-aile-est"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/285174couloirbackground.jpg')");
+
+                }else if (location.pathname=="/f3-r01"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/765742Salledecoursr1r2r3.jpg')");
+
+                }else if (location.pathname=="/f4-r02"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/765742Salledecoursr1r2r3.jpg')");
+
+                }else if (location.pathname=="/f14-r03"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/765742Salledecoursr1r2r3.jpg')");
+
+
+
+
+                }else if (location.pathname=="/f38-aile-ouest"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/285174couloirbackground.jpg')");
+
+                }else if (location.pathname=="/f15-salle-de-musique"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/787614Salledemusique.jpg')");
+
+                }else if (location.pathname=="/f16-salle-d-arts-plastiques"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/126549Salledartplastiques.jpg')");
+
+                }else if (location.pathname=="/f17-salle-de-sciences"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/991549Salledesciences.jpg')");
+
+
+
+
+                }else if (location.pathname=="/f36-bibliotheque"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/477489bibliothquebackground.jpg')");
+
+                }else if (location.pathname=="/f39-arriere-cours"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/681119arrirecours.jpg')");
+
+
+                }else if (location.pathname=="/f32-batiment-b"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/744861Refectoire.jpg')");
+
+
+                }else if (location.pathname=="/f34-infirmerie"){
+                  $("body").css("background-image", "url('http://img11.hostingpics.net/pics/475534Infirmerie.png')");
+
+
+
+                }else if (location.pathname=="/f42-terrain-de-sport"){
+                  $("body").css("background-image", "url('http://img11.hostingpics.net/pics/412710Terraindesport.png')");
+
+                }else if (location.pathname=="/f41-entrepot"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/975686Entrepot.jpg')");
+
+
+
+
+                }else if (location.pathname=="/f35-baraquements"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/934422Campement.jpg')");
+
+                }else if (location.pathname=="/f19-chambres"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/235652Chambre.jpg')");
+
+
+
+
+                }else if (location.pathname=="/f33-les-allentours-de-l-imperium"){
+                  $("body").css("background-image", "url('http://i39.servimg.com/u/f39/11/74/65/03/foryt11.jpg')");
+
+                }else if (location.pathname=="/f40-lac"){
+                  $("body").css("background-image", "url('http://img11.hostingpics.net/pics/406536Fondbase.png')");
+
+                }else if (location.pathname=="/f18-cabanon-abandonne"){
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/780182cabanon.jpg')");
+
+
+
+                }else{
+                  $("body").css("background-image", "url('http://img15.hostingpics.net/pics/144027Backgroundcard.png')");
+                }
+        });
+
+
+
+document.body.appendChild(p);
